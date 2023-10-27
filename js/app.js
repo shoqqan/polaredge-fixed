98f33a88d296df1217a816f51436356a8b7ac059
Shoqan <tataev.shokan@gmail.com>
2023-10-27 23:18:43 +0600

    created mobile version of borehole

--- js/app.js
+++ js/app.js
@@ -18,11 +18,10 @@ const postData = async (url = '', data = {}) => {
 //     smooth: 1.5,
 //     effects: true
 //   })
-//   //scrolltrigger for advantages   
 //   const cards = gsap.utils.toArray('.advantage__card');
 //   ScrollTrigger.create({
 //     pin: ".advantages"
-
+//
 //   })
 //   cards.forEach((card, index) => {
 //     gsap.to(card, {
@@ -32,7 +31,7 @@ const postData = async (url = '', data = {}) => {
 //         trigger: card,
 //         start: 'bottom bottom',
 //         end: 'center center',
-//         scrub: 1, 
+//         scrub: 1,
 //       },
 //     });
 //   });
@@ -45,42 +44,41 @@ document.addEventListener('DOMContentLoaded', function () {
     const button = question.querySelector('.question__button');
 
     button.addEventListener('click', function () {
-      question.classList.toggle('question__active');
+      question.classList.toggle('question__active')
+      button.classList.toggle('button__active')
     });
   });
 });
 
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
 });
 
-const formElement = document.getElementById('application_form'); // извлекаем элемент формы
-formElement.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(formElement); // создаём объект FormData, передаём в него элемент формы
-  // теперь можно извлечь данные
-  const name = formData.get('name'); // 'John'
-  const select = formData.get('select'); // 'Smith'
-  const phone = formData.get('phone');
-  const comments = formData.get('comments');
-  postData("http://localhost:3000/mail", { name, select, phone, comments })
-});
+if (document.getElementById('application_form')){
+  const formElement = document.getElementById('application_form');
+  formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(formElement);
+    const name = formData.get('name');
+    const select = formData.get('select');
+    const phone = formData.get('phone');
+    const comments = formData.get('comments');
+    postData("http://localhost:3000/mail", { name, select, phone, comments })
+  });
+}
+
