48b8fca59215a9a30c1909dc611dbcb91dc93430
Shoqan <tataev.shokan@gmail.com>
2023-11-06 23:54:40 +0600

    change layout and css files

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function extractBaseURL(url) {
 }
 
 const changeURL = (url) => {
-    window.location.href = window.location.href.includes("localhost")?extractBaseURL(window.location.href)+"polaredge/" + url:"https://shoqqan.github.io/polaredge-fixed/"+url;
+    window.location.href = window.location.href.includes("localhost") ? extractBaseURL(window.location.href) + "polaredge/" + url : "https://shoqqan.github.io/polaredge-fixed/" + url;
 }
 const postData = async (url = '', data = {}) => {
     const response = await fetch(url, {
@@ -20,46 +20,130 @@ const postData = async (url = '', data = {}) => {
     return response.json();
 }
 
-// if (ScrollTrigger.isTouch !== 1) {
-//   gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-//   ScrollSmoother.create({
-//     wrapper: '.wrapper',
-//     content: '.content',
-//     smooth: 1.5,
-//     effects: true
-//   })
-//   const cards = gsap.utils.toArray('.advantage__card');
-//   ScrollTrigger.create({
-//     pin: ".advantages"
-//
-//   })
-//   cards.forEach((card, index) => {
-//     gsap.to(card, {
-//       yPercent: -100 * index,
-//       ease: 'none',
-//       scrollTrigger: {
-//         trigger: card,
-//         start: 'bottom bottom',
-//         end: 'center center',
-//         scrub: 1,
-//       },
-//     });
-//   });
-// }
+const showAnim = gsap.from('.header-edge', {
+    yPercent: -100,
+    paused: true,
+    duration: 0.2
+}).progress(1);
 
-document.addEventListener('DOMContentLoaded', function () {
-    const questions = document.querySelectorAll('.question');
+ScrollTrigger.create({
+    start: "top top",
+    end: 99999,
+    onUpdate: (self) => {
+        const container = document.querySelector('.header-edge');
+        if (self.direction === -1) {
+            showAnim.play();
+            container.classList.add('fixed-header');  // добавить класс при прокрутке вверх
+        } else {
+            showAnim.reverse();
+            container.classList.remove('fixed-header');  // удалить класс при прокрутке вниз
+        }
+    }
+});
 
-    questions.forEach((question) => {
-        const button = question.querySelector('.question__button');
+gsap.registerPlugin(ScrollTrigger);
 
-        button.addEventListener('click', function () {
-            question.classList.toggle('question__active')
-            button.classList.toggle('button__active')
-        });
+const blocks = document?.querySelectorAll(".card-item");
+const panelsContainer = document.querySelector(".card-swipe");
+
+blocks.forEach((block, index) => {
+    const timeline = gsap.timeline({
+        scrollTrigger: {
+            trigger: panelsContainer,
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+        },
+    });
+
+    timeline.fromTo(
+        panelsContainer,
+        {
+            opacity: 0.5,
+            scale: 1.08,
+        },
+        {
+            opacity: 1,
+            scale: 1,
+        }
+    );
+
+    timeline.to(blocks[index - 1], {
+        filter: "blur(24px)",
+        scrollTrigger: {
+            trigger: block,
+            start: "top 200px",
+            end: "top top",
+            scrub: true,
+        },
     });
 });
 
+// FAQs
+
+const accordionBtns = document.querySelectorAll(".faq-collapse");
+
+accordionBtns.forEach((accordion) => {
+    accordion.onclick = function () {
+        this.classList.toggle("is-open");
+
+        let content = this.nextElementSibling;
+        console.log(content);
+
+        if (content.style.maxHeight) {
+            //this is if the accordion is open
+            content.style.maxHeight = null;
+        } else {
+            //if the accordion is currently closed
+            content.style.maxHeight = content.scrollHeight + "px";
+            console.log(content.style.maxHeight);
+        }
+    };
+});
+
+// Создание движка
+const engine = Matter.Engine.create();
+const world = engine.world;
+
+const tags = document.querySelectorAll('.tag');
+const container = document.querySelector('.what-in');
+const containerWidth = container.offsetWidth;
+const containerHeight = container.offsetHeight;
+
+// Создание границ (стен) контейнера
+const wallOptions = { isStatic: true };
+const ground = Matter.Bodies.rectangle(containerWidth / 2, containerHeight, containerWidth, 10, wallOptions);
+const roof = Matter.Bodies.rectangle(containerWidth / 2, 0, containerWidth, 10, wallOptions);
+const leftWall = Matter.Bodies.rectangle(0, containerHeight / 2, 10, containerHeight, wallOptions);
+const rightWall = Matter.Bodies.rectangle(containerWidth, containerHeight / 2, 10, containerHeight, wallOptions);
+Matter.World.add(world, [ground, roof, leftWall, rightWall]);
+
+tags.forEach(tag => {
+    const { width, height } = tag.getBoundingClientRect();
+
+    // Создание тела для каждого тега
+    const body = Matter.Bodies.rectangle(
+        tag.offsetLeft + width / 2,
+        -height,
+        width,
+        height,
+        { restitution: 0.7 }
+    );
+
+    Matter.World.add(world, [body]);
+
+    // Обновление позиции DOM-элемента на основе физической модели
+    Matter.Events.on(engine, 'afterUpdate', function() {
+        tag.style.transform = `translate(${body.position.x - width / 2}px, ${body.position.y - height / 2}px)`;
+    });
+
+    tag.addEventListener('mouseenter', () => {
+        Matter.Body.applyForce(body, { x: 0, y: 0 }, { x: 0, y: -0.15 });
+    });
+});
+
+// Запуск движка
+Matter.Engine.run(engine);
 
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
